ed5f43f76150a72fbe488a4eae535c64a5d1fc28
Peter Petrov <pepetrov@gmail.com>
2009-11-11 17:55:17 +0100

    checking items availability first in OrderManagementService

--- aic1/src/at/tuwien/aic666/services/OrderManagementService.java
+++ aic1/src/at/tuwien/aic666/services/OrderManagementService.java
@@ -19,7 +19,6 @@ portName = "NotificationPT", serviceName = "NotificationService")
 public class OrderManagementService {
 
     private static int counter = 0;
-
     private DataBaseMock db;
 
     public OrderManagementService() {
@@ -34,23 +33,23 @@ public class OrderManagementService {
 
     public Order placeOrder(final Item[] items, final Customer customer) {
         final Order order = new Order();
-        
+
         order.setCustomer(customer);
         order.setId("order" + OrderManagementService.counter++);
         order.setOrderDate(new Date());
+
+        //check whether all items are available...
+        final Item item = this.checkItemsAvailability(items);
+        if (item != null) {
+            //Does a Soap Fault break the method like an exception or continues...?
+            throw new ItemUnavailableFault("Item with id " + item.getProductId() + "is not available");
+        }
 
         for (final Item i : items) {
             if (i.getQuantity() <= this.db.getAvailableItemsList().get(i.getProductId())) {
                 this.db.decreaseItemsAvailable(i, i.getQuantity());
                 this.db.addShippingItem(i);
-                // ?
-                //final Collection<Item> currentItems = order.getItems();
-                //currentItems.add(i);
-                //order.setItems(currentItems);
                 order.getItems().add(i);
-            } else {
-                // TODO undo db changes made so far (decreaseItemsAvailable, addShippingItem)
-                throw new ItemUnavailableFault("Item with id " + i.getProductId() + "is not available");
             }
         }
 
@@ -69,8 +68,22 @@ public class OrderManagementService {
         return finished;
     }
 
-
     public void insertTestData() {
         //TODO implement method stub...
+    }
+
+    /**
+     * Returns the first unavailable item of the passed array or null if all
+     * items are available
+     * @param items the items to check
+     * @return read javadoc....
+     */
+    private Item checkItemsAvailability(final Item[] items) {
+        for (final Item i : items) {
+            if (i.getQuantity() > this.db.getAvailableItemsList().get(i.getProductId())) {
+                return i;
+            }
+        }
+        return null;
     }
 }
